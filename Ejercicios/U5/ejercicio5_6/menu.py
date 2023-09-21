bcac4290542859719cd4012ce49c51d21fa0eddb
OmegaMLM <mateo.marchesinlujan@gmail.com>
2023-09-21 13:00:57 -0300

    Cambio en el menu con Pretty Tables

--- Ejercicios/U5/ejercicio5_6/menu.py
+++ Ejercicios/U5/ejercicio5_6/menu.py
@@ -1,15 +1,16 @@
 from utility import add_stock, consult_stock, delete_stock
-
+from prettytable import PrettyTable
 def menu():
   while True:
     try:
-      print("""
-            --- Inventario la Verdugod ---
-            1. Agregar stock
-            2. Consultar stock
-            3. Eliminar stock
-            4. Salir
-            """)
+      menu_table = PrettyTable()
+      menu_table.field_names = ["Opciones", "Descripcion"]
+      menu_table.add_row(["1", "Agregar stock"])
+      menu_table.add_row(["2", "Consultar stock"])
+      menu_table.add_row(["3", "Eliminar stock"])
+      menu_table.add_row(["4", "Salir"])
+      print(menu_table)
+      
       opcion = int(input('Ingrese una opcion: '))
       match opcion:
         case 1:
